ca633cc71154339c4ec426194504562b959ad692
Agus Siswanto <agus.siswanto@erajaya.com>
2022-01-02 20:29:26 +0700

    setting name db

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => '127.0.0.1',
             'port' => '3306',
-            'database' => 'charts',
+            'database' => 'analytics',
             'username' => 'root',
             'password' => '',
             'unix_socket' => env('DB_SOCKET', ''),
