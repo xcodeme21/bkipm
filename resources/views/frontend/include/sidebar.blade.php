815977b4e0b700e20e59f9ac192a4ef617af8078
Agus Siswanto <agus.siswanto@erajaya.com>
2022-01-23 11:03:51 +0700

    change logo

--- resources/views/frontend/include/sidebar.blade.php
+++ resources/views/frontend/include/sidebar.blade.php
@@ -3,10 +3,9 @@
 
 <!-- Sidebar - Brand -->
 <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ url('/') }}">
-    <div class="sidebar-brand-icon rotate-n-15">
-        <i class="fas fa-laugh-wink"></i>
+    <div class="sidebar-brand-icon">
+        <img src="{{ asset('public/uploads/logo/'.@$logo->logo) }}" height="50" alt="">
     </div>
-    <div class="sidebar-brand-text mx-3">{{ config('app.name') }}</div>
 </a>
 
 <!-- Divider -->
